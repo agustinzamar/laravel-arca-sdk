a6283d11797f32a0d681b524fbfdb6d25e7a5d44
Agustin Zamar <agustinzamar33@gmail.com>
2025-09-27 04:40:37 -0300

    Enhance RelatedInvoice class by making properties readonly and adding optional cuit and invoiceDate attributes

--- src/Domain/RelatedInvoice.php
+++ src/Domain/RelatedInvoice.php
@@ -5,13 +5,16 @@ declare(strict_types=1);
 namespace AgustinZamar\LaravelArcaSdk\Domain;
 
 use AgustinZamar\LaravelArcaSdk\Enums\InvoiceType;
+use Illuminate\Support\Carbon;
 
 class RelatedInvoice
 {
     public function __construct(
-        public InvoiceType $invoiceType,
-        public int         $pointOfSale,
-        public int         $invoiceNumber,
+        public readonly InvoiceType $invoiceType,
+        public readonly int         $pointOfSale,
+        public readonly int         $invoiceNumber,
+        public readonly ?string     $cuit = null,
+        public readonly ?Carbon     $invoiceDate = null,
     )
     {
     }
